a66f1ed0006c8a9c90a9b73ba3cfe53c01fb0ffc
torbjbak <tbakke93@gmail.com>
2020-10-21 14:06:26 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://mysql-ait.stud.idi.ntnu.no/torbjbak",
+                    "torbjbak","Le8OcFET");
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select forfatter, tittel from boktittel where isbn = '0-596-00123-1'");
+            while(rs.next()) {
+                System.out.println(rs.getString("forfatter") + "   " + rs.getString("tittel"));
+            }
+
+            rs = stmt.executeQuery("select count(*) antall from eksemplar where isbn = '0-596-00123-1'");
+            while(rs.next()) {
+                System.out.println(rs.getInt("antall"));
+            }
+
+            con.close();
+
+        }catch(Exception ex) {
+            // If connection fail
+            System.out.println("Error: " + ex);
+        }
+    }
+}
