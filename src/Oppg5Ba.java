be11fbaed8dfe3eae4a684add9ab0c866fe44285
torbjbak <tbakke93@gmail.com>
2020-10-22 13:19:55 +0200

    Updated 5B a

--- src/Oppg5Ba.java
+++ src/Oppg5Ba.java
@@ -3,9 +3,12 @@ import java.sql.*;
 
 public class Oppg5Ba {
 
-    public static void main(String[] args) {
-        try {
 
+
+    public static void main(String[] args) {
+        String isbn = "'0-596-00123-1'";
+
+        try {
             Class.forName("com.mysql.jdbc.Driver");
 
             Connection con = DriverManager.getConnection("jdbc:mysql://mysql-ait.stud.idi.ntnu.no/torbjbak",
@@ -13,22 +16,33 @@ public class Oppg5Ba {
 
             Statement stmt = con.createStatement();
 
-            System.out.println("Forfatter           Tittel");
-            ResultSet rs = stmt.executeQuery("select forfatter, tittel from boktittel where isbn = '0-596-00123-1'");
-            while(rs.next()) {
-                System.out.println(rs.getString("forfatter") + "   " + rs.getString("tittel"));
-            }
+            ResultSet rs = stmt.executeQuery("select forfatter, tittel from boktittel where isbn = " + isbn);
+            printTable(rs);
+            System.out.println();
 
-            rs = stmt.executeQuery("select count(*) antall from eksemplar where isbn = '0-596-00123-1'");
-            while(rs.next()) {
-                System.out.println("\nAntall: "+ rs.getInt("antall"));
-            }
+            rs = stmt.executeQuery("select count(*) antall from eksemplar where isbn = " + isbn);
+            printTable(rs);
 
             con.close();
 
         }catch(Exception ex) {
             // If connection fail
             System.out.println("Error: " + ex);
+        }
+    }
+
+    static void printTable(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int i;
+        for(i = 1; i <= md.getColumnCount(); i++) {
+            System.out.printf("|%20S|", md.getColumnName(i));
+        }
+        System.out.println();
+        while(rs.next()) {
+            for(i = 1; i <= md.getColumnCount(); i++) {
+                System.out.printf("|%20s|", rs.getString(i));
+            }
+            System.out.println();
         }
     }
 }
--- src/Oppg5Bb.java
+++ src/Oppg5Bb.java
@@ -1,23 +1,13 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Oppg5Bb {
 
-    void printTable(ResultSet rs) throws SQLException {
-        ResultSetMetaData md = rs.getMetaData();
-        int i;
-        for(i = 1; i <= md.getColumnCount(); i++) {
-            System.out.printf("%16s", md.getColumnName(i));
-        }
-        System.out.println();
-        while(rs.next()) {
-            for(i = 1; i <= md.getColumnCount(); i++) {
-                System.out.printf("%16s", rs.getString(i));
-            }
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
-        System.out.printf("%20s", "text");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Registrer lånetaker\nEksemplarnr: ");
+        String eksNr = sc.nextLine();
+        System.out.println("");
     }
 }
